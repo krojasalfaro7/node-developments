b6abe8c88e50385009d7f8501e489bca69e37791
Kevin Rojas <kevin@roke.ubbedigital.com>
2021-06-08 22:48:54 -0400

    eco

--- apirest/src/controllers/eco.ts
+++ apirest/src/controllers/eco.ts
@@ -4,6 +4,6 @@ import { Request, Response } from 'express';
 // Eco
 export const eco = async(req: Request, res: Response) => {
 
-    console.log(req.body)
-        res.json(req.body);        
+    console.log(req.body);
+    res.json(req.body);        
 }
